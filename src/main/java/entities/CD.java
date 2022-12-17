dad5a0e6f7c32f20af3fc341173912acc202e4dc
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-12-17 15:34:05 -0500

    updated

--- src/main/java/entities/CD.java
+++ src/main/java/entities/CD.java
@@ -10,11 +10,57 @@ public class CD {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToMany()
-    private Set<artist> artist = new HashSet<artist>();
+    public String getTitle() {
+        return title;
+    }
 
-    @ManyToMany()
-    private Set<artist> artists = new HashSet<artist>();
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    private String title;
+    private String description;
+    private String year;
+    private String artist;
+    private int price;
+
+//    @OneToMany()
+//    private Set<artist> artist = new HashSet<artist>();
+//
+//    @ManyToMany()
+//    private Set<artist> artists = new HashSet<artist>();
 
     public CD(Long id) {
         this.id = id;
